9ffbc0065b4ce4bb560419cd41052c6ef8d6774f
Jasmine Kaur Singh <60487022+jasminekaursingh@users.noreply.github.com>
2022-10-05 12:11:41 -0700

    added in jsoup demo

--- bronco-projects-backend/src/main/java/com/binarybeasts/broncoprojectsbackend/BroncoProjectsBackendApplication.java
+++ bronco-projects-backend/src/main/java/com/binarybeasts/broncoprojectsbackend/BroncoProjectsBackendApplication.java
@@ -4,11 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.google.common.base.Preconditions;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 @SpringBootApplication
 public class BroncoProjectsBackendApplication {
     public static void main(String[] args) {
         Preconditions.checkNotNull("A", "String must not be null!");
         SpringApplication.run(BroncoProjectsBackendApplication.class, args);
-    }
-
+        
+        //Jsoup demo
+        Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
+        System.out.println(doc.title());
+        Elements newsHeadlines = doc.select("#mp-itn b a");
+        for (Element headline : newsHeadlines) {
+            System.out.println("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
+        }
 }
