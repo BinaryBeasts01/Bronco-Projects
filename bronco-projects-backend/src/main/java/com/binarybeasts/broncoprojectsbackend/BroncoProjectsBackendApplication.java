4ea610c3af70381a8566bfd27cc7e8508fca7dcd
Austin Lee <austinslee02@gmail.com>
2022-10-04 23:04:05 -0700

    used precondition checkNotNull from google guava

--- bronco-projects-backend/src/main/java/com/binarybeasts/broncoprojectsbackend/BroncoProjectsBackendApplication.java
+++ bronco-projects-backend/src/main/java/com/binarybeasts/broncoprojectsbackend/BroncoProjectsBackendApplication.java
@@ -2,12 +2,13 @@ package com.binarybeasts.broncoprojectsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.google.common.base.Preconditions;
 
 @SpringBootApplication
 public class BroncoProjectsBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BroncoProjectsBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Preconditions.checkNotNull("A", "String must not be null!");
+        SpringApplication.run(BroncoProjectsBackendApplication.class, args);
+    }
 
 }
