85be23c2dab45bc4b9fbabe1dc18bf0607abfdf9
Viswadeep Manam <manam.viswadeep@gmail.com>
2022-10-22 01:10:15 -0700

    Update BroncoProjectsBackendApplication.java

--- bronco-projects-backend/src/main/java/com/binarybeasts/broncoprojectsbackend/BroncoProjectsBackendApplication.java
+++ bronco-projects-backend/src/main/java/com/binarybeasts/broncoprojectsbackend/BroncoProjectsBackendApplication.java
@@ -11,16 +11,6 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 @SpringBootApplication
 public class BroncoProjectsBackendApplication {
     public static void main(String[] args)  throws IOException {
-        Preconditions.checkNotNull("A", "String must not be null!");
         SpringApplication.run(BroncoProjectsBackendApplication.class, args);
-        
-        //Jsoup demo
-        Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
-        System.out.println(doc.title());
-        Elements newsHeadlines = doc.select("#mp-itn b a");
-        for (Element headline : newsHeadlines) {
-            System.out.println(headline.attr("title") + ", " + headline.absUrl("href"));
-        }
-    
     }
 }
