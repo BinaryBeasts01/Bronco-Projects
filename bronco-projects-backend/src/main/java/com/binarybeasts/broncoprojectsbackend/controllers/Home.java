64deb69f62958a0ea54851577aabdde45e441bd9
SeveralCube22 <manam.viswadeep@gmail.com>
2022-11-22 12:24:33 -0800

    Connected project route with backend.

--- bronco-projects-backend/src/main/java/com/binarybeasts/broncoprojectsbackend/controllers/Home.java
+++ bronco-projects-backend/src/main/java/com/binarybeasts/broncoprojectsbackend/controllers/Home.java
@@ -2,10 +2,12 @@ package com.binarybeasts.broncoprojectsbackend.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class Home {
-    @GetMapping("/")
+    @RequestMapping(value={"/", "/projects/*"}, method= RequestMethod.GET)
     public String getHome() {
         return "index.html";
     }
