c4ce17bf1ab19274500462da214b7649e182ca13
SeveralCube22 <manam.viswadeep@gmail.com>
2022-11-22 20:45:55 -0800

    Fix.

--- bronco-projects-backend/src/main/java/com/binarybeasts/broncoprojectsbackend/controllers/Home.java
+++ bronco-projects-backend/src/main/java/com/binarybeasts/broncoprojectsbackend/controllers/Home.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class Home {
-    @RequestMapping(value={"/", "/projects/*"}, method= RequestMethod.GET)
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" }, method = RequestMethod.GET)
     public String getHome() {
         return "index.html";
     }
