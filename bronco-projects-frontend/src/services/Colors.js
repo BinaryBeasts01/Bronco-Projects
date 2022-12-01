86e62c1189f4f48304838551e670f7477304477f
SeveralCube22 <manam.viswadeep@gmail.com>
2022-11-30 23:08:26 -0800

    Remove colorthief.

--- bronco-projects-frontend/src/services/Colors.js
+++ bronco-projects-frontend/src/services/Colors.js
@@ -1,13 +1,11 @@
-import ColorThief from "colorthief";
-
 export const getDominantColor = (base64, isPng) => {
     return new Promise((resolve, reject) => {
         const image = new Image();
         image.src = `data:image/${isPng ? "png" : "jpeg"};base64, ${base64}`;
 
         image.onload = function () {
-            const colorThief = new ColorThief();
-            const color = colorThief.getColor(this, 10);
+            //const colorThief = new ColorThief();
+            const color = [0, 0, 0];
             resolve(color);
         }
     });
