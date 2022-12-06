d200fbefb4f5fbeed0f389fe9582074440d11396
Austin Lee <austinslee02@gmail.com>
2022-12-05 20:20:29 -0800

    created project create modal

--- bronco-projects-frontend/src/components/ActionCenter.js
+++ bronco-projects-frontend/src/components/ActionCenter.js
@@ -1,11 +1,15 @@
 import React, {useState, useEffect} from "react";
 import {Button, Card, ListGroup, ListGroupItem} from "react-bootstrap";
 import {useNavigate} from "react-router-dom";
+import ProjectCreateForm from "./ProjectCreateForm";
 
 const ActionCenter = ({email}) => {
     const navigate = useNavigate();
 
     const [openCreatedProjects, setOpenCreatedProjects] = useState(false);
+    const [projectCreateFormVisible, setProjectCreateFormVisible] = useState(false);
+
+    let projectCreateModal = <ProjectCreateForm showProjectCreateForm={projectCreateFormVisible} closeProjectCreateModal={() => setProjectCreateFormVisible(false)}/>
 
     useEffect(() => {
         if(openCreatedProjects)
@@ -15,14 +19,17 @@ const ActionCenter = ({email}) => {
 
 
     return (
+        <div>
           <ListGroup horizontal={true} style={{paddingBottom: "10%"}}>
               <ListGroupItem style={{backgroundColor: "rgb(3,3,3)"}}>
-                  <Button>Create Project</Button>
+                  <Button onClick={() => setProjectCreateFormVisible(true)}>Create Project</Button>
               </ListGroupItem>
               <ListGroupItem style={{backgroundColor: "rgb(3,3,3)"}}>
                   <Button onClick={() => setOpenCreatedProjects(true)}>View Created Projects</Button>
               </ListGroupItem>
           </ListGroup>
+            {projectCreateModal}
+        </div>
     );
 }
 
--- bronco-projects-frontend/src/services/ProjectsService.js
+++ bronco-projects-frontend/src/services/ProjectsService.js
@@ -1,6 +1,8 @@
 import API_URL from "../constants/API_URL";
 import axios from "axios";
 import AuthService from "./AuthService";
+import FormData from "form-data";
+import HttpStatusCodes from "http-status-codes";
 
 const PROJECTS_URL = API_URL + "projects/";
 
@@ -22,6 +24,36 @@ class ProjectsService {
                 console.log("PROJECTS")
                 console.log(response.data)
                 return response.data
+            });
+    }
+    static createProject(name, description, image, tags, department) {
+        const FormData = require('form-data');
+        let url = PROJECTS_URL + "project";
+
+        let data = new FormData();
+        data.append("name", name);
+        data.append("description", description);
+        data.append("image", image);
+        data.append("tags", tags);
+        data.append("department", department);
+        console.log(data)
+        let config = {
+            method: 'post',
+            url: url,
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                ...AuthService.authHeader()
+            },
+            data : data
+        };
+        return axios(config)
+            .then((response) => {
+                if (response.status === HttpStatusCodes.OK) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
             });
     }
 
